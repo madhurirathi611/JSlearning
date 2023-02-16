edf2f1619f3974b0e0157b05b176d54411820e20
madhurirathi611 <madhurirathi611@gmail.com>
2023-02-16 08:56:46 +0530

    variables first assignment done

---
+++ 01_variable_assign.js
@@ -0,0 +1,22 @@
+console.log("**** 1.****");
+var myFullName;
+console.log(myFullName);
+myFullName="Mohit Sharma";
+console.log(myFullName);
+myFullName="Mohit Rahul Sharma";
+console.log(myFullName);
+
+console.log("**** 2.****");
+var myLove="I Love only JS";
+console.log(myLove);
+
+console.log("**** 3.****");
+var firstName="Mohit";
+var lastName="Sharma";
+var age=31;
+var address="Pune";
+var pincode=431048;
+var gender= "Male";
+var isMarried="Yes";
+console.log(firstName,lastName,age,address,pincode,gender,isMarried);
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,19 @@
 console.log("Madhuri Rathi");
+console.log('31');
+console.log("Pune");
+
+//console.log("working on javascript");
+/*how to write multipe commments in Js
+on next line */
+
+var fullName="Madhuri Rathi";
+console.log(fullName);
+console.log(fullName);
+
+var age=31;
+console.log(age);
+
+var city; //variable declearation
+city="Mumbai";
+console.log(city);
+
