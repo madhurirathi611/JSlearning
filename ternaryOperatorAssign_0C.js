7110235e3e22210a4d4d53e928f05da8b3bd0c6a
madhurirathi611 <madhurirathi611@gmail.com>
2023-03-08 16:39:04 +0530

    Update ternaryOperatorAssign_0C.js

--- ternaryOperatorAssign_0C.js
+++ ternaryOperatorAssign_0C.js
@@ -1,7 +1,9 @@
 console.log("-------TCS interview eligibility result--------");
 function companyTest(gradScore,hscScore,sscScore,candidateName)
 {
-var result=gradScore>=70 || hscScore>=80 || sscScore>=90 ?`Congrates ${candidateName} you are eligible for TCS interview.`:`Sorry ${candidateName},Unfortunately you are not eligible for interview.`;
+var result=gradScore>=70 || hscScore>=80 || sscScore>90 ?
+`Congrates ${candidateName} you are eligible for TCS interview.`
+:`Sorry ${candidateName},Unfortunately you are not eligible for interview.`;
 console.log(result);
 }
 companyTest(80,86,90,"Alice");
